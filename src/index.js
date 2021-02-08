9538a2c15faa9eef6298daade26fe07ade307c70
Yordan <yordansaldanabarrera@gmail.com>
2021-02-07 22:02:34 -0300

    REDUX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import REACTDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
-REACTDOM.render(
-    <App/>,document.getElementById('root')
+import App from './component/App';
+import reducers from './reducers';
+
+const store=createStore(reducers,applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
