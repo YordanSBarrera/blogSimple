b237f44da29d7b2049f94723bcd76c5ade5c04b3
Yordan SB <yordansaldanabarrera@gmail.com>
2021-02-10 19:04:28 -0300

    optimisando las consultas OK

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,13 +7,16 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-/*
+
 export const fetchUser = id => async dispatch => {
-    const response = await jsonPlaceholder.get('/users/' + id);
+    const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response.data })
 }
-*/
 
+
+
+/****
+ * //....solucion usando LODASH...
 export const fetchUser = id => dispatch => {
     _fetchUser(id, dispatch)
 }
@@ -22,3 +25,19 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
     dispatch({ type: 'FETCH_USER', payload: response.data })
 }
 ); 
+
+*/
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    //console.log(getState())
+
+    // const userIds=_.uniq(_.map(getState().posts,'userId'))
+    // userIds.forEach(id=>dispatch(fetchUser(id)));  
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
--- src/component/UserHeader.js
+++ src/component/UserHeader.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchUser } from '../actions'
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
 
     render() {
         const user = this.props.users.find(user => user.id === this.props.userId);
@@ -23,4 +19,4 @@ const mapStateToProps = (state) => {
     return { users: state.users }
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
